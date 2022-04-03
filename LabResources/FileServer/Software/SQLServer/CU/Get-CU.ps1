b98ce9db6fc5f516092b511a6908aee19e77e300
andreasjordan <anj@ordix.de>
2022-04-03 17:33:19 +0200

    initial commit

---
+++ LabResources/FileServer/Software/SQLServer/CU/Get-CU.ps1
@@ -0,0 +1,77 @@
+﻿[CmdletBinding()]
+param (
+    [ValidateSet('2019', '2017', '2016', '2014')]
+    [string[]]$Version = @('2019', '2017'),
+    [int]$Last = 1,
+    [string]$Path = '.',
+    [switch]$UpdateBuildReference = $true
+)
+
+function Get-CU {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateSet('2019', '2017', '2016', '2014')]
+        [string]$Version,
+        [regex]$BuildBaseRegex,
+        [int]$Last = 1,
+        [string[]]$Exclude,
+        [string]$Path = '.'
+    )
+    if ($null -eq $BuildBaseRegex) {
+        $BuildBaseRegex = switch ($Version) {
+            '2019' { '^15' }
+            '2017' { '^14' }
+            '2016' { '^13.0.5' }  # Based on SP2
+            '2014' { '^12.0.6' }  # Based on SP3
+        }
+    }
+    if ($Version -eq '2019') { 
+        $Exclude += 'CU7'  # CU7 is not available any more
+    }
+    $buildrefFile = Join-Path -Path (Get-DbatoolsConfigValue -Name 'Path.DbatoolsData') -ChildPath "dbatools-buildref-index.json"
+    $buildrefData = (Get-Content -Path $buildrefFile -Raw | ConvertFrom-Json).Data
+    $cuData = $buildrefData | 
+        Where-Object -FilterScript { $_.Version -match $BuildBaseRegex -and $_.CU -ne $null -and $_.CU -notin $Exclude } |
+        Sort-Object -Property KBList |
+        Select-Object -Last $Last
+    foreach ($cu in $cuData) {
+        $kbNr = $cu.KBList
+        $cuName = $cu.CU
+        $filePath = Join-Path -Path $Path -ChildPath "SQLServer$Version-KB$kbNr-$cuName-x64.exe"
+        if (-not (Test-Path -Path $filePath)) {
+            Write-Progress -Activity "Downloading Cumulative Updates for SQL Server" -Status "Downloading KB $kbNr for SQL Server $Version to $filePath"
+            Save-DbaKbUpdate -Name $kbNr -FilePath $filePath
+        }
+    }
+}
+
+Write-Progress -Activity "Downloading Cumulative Updates for SQL Server" -Status "Importing dbatools"
+Import-Module -Name dbatools
+
+if ($UpdateBuildReference) {
+    Write-Progress -Activity "Downloading Cumulative Updates for SQL Server" -Status "Updating build reference"
+    Update-DbaBuildReference
+}
+
+foreach ($ver in $Version) {
+    Get-CU -Version $ver -Last $Last -Path $Path
+}
+
+Write-Progress -Activity "Downloading Cumulative Updates for SQL Server" -Completed
+
+
+<# Other usage examples:
+
+Get-CU -Version 2019 -Path 'C:\SQLServerPatches'
+Get-CU -Version 2017 -Path 'C:\SQLServerPatches' -Last 5
+Get-CU -Version 2016 -Path 'C:\SQLServerPatches' -Last 5
+Get-CU -Version 2014 -Path 'C:\SQLServerPatches'
+# KB4583462 - Security update for SQL Server 2014 SP3 CU4: January 12, 2021
+Save-DbaKbUpdate -Name 4583462 -FilePath 'C:\SQLServerPatches\SQLServer2014-KB4583462-CU4-Security-x64.exe'
+# KB4583465 - Security update for SQL Server 2012 SP4 GDR: January 12, 2021
+Save-DbaKbUpdate -Name 4583465 -FilePath 'C:\SQLServerPatches\SQLServer2012-KB4583465-GDR-Security-x64.exe'
+# Check for new versions at: https://sqlserverbuilds.blogspot.com/
+# KB5003830 - New CU that is not yet known by dbatools
+Save-DbaKbUpdate -Name 5003830 -FilePath 'C:\SQLServerPatches\SQLServer2017-KB5003830-CU25-x64.exe'
+
+#>
---
+++ LabResources/InitScript01.ps1
@@ -0,0 +1,54 @@
+$ErrorActionPreference = 'Stop'
+
+Import-Module -Name ActiveDirectory
+Import-Module -Name DnsServer
+
+$pw = ConvertTo-SecureString -String 'P@ssw0rd' -AsPlainText -Force
+
+$ouAdminComputer = New-ADOrganizationalUnit -Name 'AdminComputer' -PassThru
+$ouAdminUser = New-ADOrganizationalUnit -Name 'AdminUser' -PassThru
+$ouSqlComputer = New-ADOrganizationalUnit -Name 'SqlComputer' -PassThru
+$ouSqlUser = New-ADOrganizationalUnit -Name 'SqlUser' -PassThru
+
+New-ADUser -Name SQLServer -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLSrv1 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLSrv2 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLSrv3 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLSrv4 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLSrv5 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLAdmin -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLUser1 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLUser2 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLUser3 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLUser4 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+New-ADUser -Name SQLUser5 -AccountPassword $pw -Enabled $true -Path $ouSqlUser.DistinguishedName
+
+New-ADGroup -Name SQLServiceAccounts -GroupCategory Security -GroupScope Global -Path $ouSqlUser.DistinguishedName
+New-ADGroup -Name SQLAdmins -GroupCategory Security -GroupScope Global -Path $ouSqlUser.DistinguishedName
+New-ADGroup -Name SQLUsers -GroupCategory Security -GroupScope Global -Path $ouSqlUser.DistinguishedName
+
+Add-ADGroupMember -Identity SQLServiceAccounts -Members SQLServer, SQLSrv1, SQLSrv2, SQLSrv3, SQLSrv4, SQLSrv5
+Add-ADGroupMember -Identity SQLAdmins -Members SQLAdmin
+Add-ADGroupMember -Identity SQLUsers -Members SQLUser1, SQLUser2, SQLUser3, SQLUser4, SQLUser5
+
+
+<#
+$session = New-PSSession -ComputerName dc
+Invoke-Command -Session $session -ScriptBlock { Get-Disk | Where-Object IsOffline | Set-Disk -IsOffline $false }
+Invoke-Command -Session $session -ScriptBlock { Get-Disk | Where-Object IsReadOnly | Set-Disk -IsReadOnly $false }
+Invoke-Command -Session $session -ScriptBlock { $null = New-SmbShare -Path D:\FileServer -Name FileServer | Grant-SmbShareAccess -AccountName "ORDIX\Admin" -AccessRight Full -Force }
+Invoke-Command -Session $session -ScriptBlock { $null = New-SmbShare -Path D:\FileServer\Backup -Name Backup | Grant-SmbShareAccess -AccountName "ORDIX\Admin" -AccessRight Full -Force }
+Invoke-Command -Session $session -ScriptBlock { $null = New-SmbShare -Path D:\FileServer\ORDIX -Name ORDIX | Grant-SmbShareAccess -AccountName "ORDIX\Admin" -AccessRight Full -Force }
+Invoke-Command -Session $session -ScriptBlock { $null = New-SmbShare -Path D:\FileServer\SampleDatabases -Name SampleDatabases | Grant-SmbShareAccess -AccountName "ORDIX\Admin" -AccessRight Full -Force }
+Invoke-Command -Session $session -ScriptBlock { $null = New-SmbShare -Path D:\FileServer\Software -Name Software | Grant-SmbShareAccess -AccountName "ORDIX\Admin" -AccessRight Full -Force }
+Invoke-Command -Session $session -ScriptBlock { $null = Grant-SmbShareAccess -Name Backup -AccountName "ORDIX\SQLServiceAccounts" -AccessRight Change -Force }
+
+Add-DnsServerResourceRecordCName -ComputerName dc -ZoneName ordix.local -HostNameAlias dc.ordix.local -Name fs
+#>
+
+Get-ChildItem -Path D:\GitHub\demos\LabResources\GPO\ | ForEach-Object -Process {
+    $id = (Get-ChildItem -Path $_.FullName).Name
+    $null = New-GPO -Name $_.Name
+    $null = Import-GPO -TargetName $_.Name -Path $_.FullName -BackupId $id
+    $null = New-GPLink -Name $_.Name -Target (Get-ADRootDSE).defaultNamingContext
+}
