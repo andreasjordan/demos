24a7d776a62ddf77ea1fc3310e8d107c2480c437
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2023-01-30 18:02:34 +0100

    New version with less code

--- dbatools/downgrade_database.ps1
+++ dbatools/downgrade_database.ps1
@@ -13,7 +13,7 @@ Import-Module -Name dbatools
 
 # Connect to the source database.
 # You may have to add -SqlCredential if using integrated security is not possible.
-$sourceDatabase = Get-DbaDatabase -SqlInstance SQL01\SQL2022 -Database StackOverflowMini -SqlCredential
+$sourceDatabase = Get-DbaDatabase -SqlInstance SQL01\SQL2022 -Database StackOverflowMini
 
 # Connect to the target instance.
 # You may have to add -SqlCredential if using integrated security is not possible.
@@ -30,20 +30,11 @@ $targetDatabaseParams = @{
 # Create the new database on the target instance.
 $null = New-DbaDatabase @targetDatabaseParams
 
-# Loop through all the tables.
-$copyResults = foreach ($table in $sourceDatabase.Tables) {
-    # Get the CREATE TABLE script. The method also generated two SET commands so we join them all together.
-    $query = $table.Script() -join "`n"
-
-    # Run the CREATE TABLE script against the target database.
-    Invoke-DbaQuery -SqlInstance $targetServer -Database $sourceDatabase.Name -Query $query
-
-    # Refresh the SMO so that it knows the new table.
-    $targetServer.Databases[$sourceDatabase.Name].Tables.Refresh()
-
-    # Copy the data over to the target table.
-    $table | Copy-DbaDbTableData -Destination $targetServer -DestinationDatabase $sourceDatabase.Name -Table $table.Name
+# Copy all the tables including the data.
+$transferParams = @{
+    SqlInstance            = $sourceDatabase.Parent
+    DestinationSqlInstance = $targetServer
+    Database               = $sourceDatabase.Name
+    CopyAll                = 'Tables'
 }
-
-# View the results to see how much data was copied and in what time.
-$copyResults | Format-Table -Property SourceTable, RowsCopied, Elapsed
+Invoke-DbaDbTransfer @transferParams
