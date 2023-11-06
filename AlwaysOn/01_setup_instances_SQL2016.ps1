dec4c4c5e96e5acc94ed722a6ae3ae503c8649c8
Andreas Jordan <anj@ordix.de>
2023-11-06 19:47:24 +0100

    Also test SQL Server 2016

---
+++ AlwaysOn/01_setup_instances_SQL2016.ps1
@@ -0,0 +1,71 @@
+﻿[CmdletBinding()]
+param (
+    [string]$DomainName = 'ORDIX',
+    [string]$DomainController = 'DC',
+    [string[]]$ClusterNodes = @('SQL01', 'SQL02'),
+    [string[]]$SqlInstances = @('SQL01\SQL2016', 'SQL02\SQL2016'),
+    [string]$SQLServerServiceAccount = 'gMSA-SQLServer',
+    [SecureString]$AdminPassword = (ConvertTo-SecureString -String 'P@ssw0rd' -AsPlainText -Force),
+    [string]$SQLServerSourcesPath = '\\fs\Software\SQLServer\ISO',
+    [string]$SQLServerPatchesPath = '\\fs\Software\SQLServer\CU',
+    [string]$SampleDatabasesPath = '\\fs\SampleDatabases',
+    [string]$DatabaseName = 'AdventureWorks'
+)
+
+$ErrorActionPreference = 'Stop'
+
+Import-Module -Name PSFramework
+Import-Module -Name dbatools
+
+$PSDefaultParameterValues['*-Dba*:EnableException'] = $true
+$PSDefaultParameterValues['*-Dba*:Confirm'] = $false
+
+try {
+
+$administratorCredential = [PSCredential]::new("$DomainName\Admin", $AdminPassword)
+$sqlServerCredential = [PSCredential]::new("$DomainName\$SQLServerServiceAccount$", [SecureString]::new())
+
+if ($null -eq (Get-ADServiceAccount -Filter "Name -eq '$SQLServerServiceAccount'") ) {
+    throw "Service Account '$SQLServerServiceAccount' for SQL Server not found. Creation of this account is not part of this script but done in https://github.com/andreasjordan/demos/blob/master/AutomatedLab/CustomScripts/SQLServer_AlwaysOn.ps1"
+}
+
+Write-PSFMessage -Level Host -Message 'Change powerplan of cluster nodes to high performance'
+Set-DbaPowerPlan -ComputerName $ClusterNodes | Format-Table
+
+Write-PSFMessage -Level Host -Message 'Install SQL Server instances on cluster nodes'
+$installParams = @{
+    SqlInstance        = $SqlInstances
+    Version            = 2016
+    Feature            = 'Engine'
+    Path               = $SQLServerSourcesPath
+    UpdateSourcePath   = $SQLServerPatchesPath
+    EngineCredential   = $sqlServerCredential
+    AgentCredential    = $sqlServerCredential
+    AuthenticationMode = 'Mixed'
+    Credential         = $administratorCredential
+    Restart            = $true
+    EnableException    = $false
+}
+$installResult = Install-DbaInstance @installParams
+$installResult | Format-Table
+if ($false -in $installResult.Successful) {
+    throw "Install-DbaInstance not successful"
+}
+
+Write-PSFMessage -Level Host -Message 'Grant instant file initialization rights to SQL Server service account on cluster nodes'
+Set-DbaPrivilege -ComputerName $ClusterNodes -Type IFI
+
+Write-PSFMessage -Level Host -Message 'Configure SQL Server instances: MaxMemory / MaxDop / CostThresholdForParallelism'
+Set-DbaMaxMemory -SqlInstance $SqlInstances -Max 2048 | Format-Table
+Set-DbaMaxDop -SqlInstance $SqlInstances | Format-Table
+Set-DbaSpConfigure -SqlInstance $SqlInstances -Name CostThresholdForParallelism -Value 50 | Format-Table
+
+Write-PSFMessage -Level Host -Message 'Restore and configure demo database'
+$null = Restore-DbaDatabase -SqlInstance $SqlInstances[0] -Path "$SampleDatabasesPath\AdventureWorks2016.bak" -DatabaseName $DatabaseName
+$null = Set-DbaDbOwner -SqlInstance $SqlInstances[0] -Database $DatabaseName -TargetLogin sa
+$null = Set-DbaDbRecoveryModel -SqlInstance $SqlInstances[0] -Database $DatabaseName -RecoveryModel Full
+$null = Backup-DbaDatabase -SqlInstance $SqlInstances[0] -Database $DatabaseName
+
+Write-PSFMessage -Level Host -Message 'finished'
+
+} catch { Write-PSFMessage -Level Warning -Message 'failed' -ErrorRecord $_ }
---
+++ AlwaysOn/02_setup_availability_group_SQL2016.ps1
@@ -0,0 +1,80 @@
+﻿[CmdletBinding()]
+param (
+    [string[]]$SqlInstances = @('SQL01\SQL2016', 'SQL02\SQL2016'),
+    [string]$BackupPath = '\\fs\Backup',
+    [string]$DatabaseName = 'AdventureWorks',
+    [string]$AvailabilityGroupName = 'AdventureSQL2016',
+    [System.Net.IPAddress]$AvailabilityGroupIP = '192.168.3.73',
+    [string[]]$EndpointUrls = @('TCP://192.168.3.31:5025', 'TCP://192.168.3.32:5025')
+)
+
+$ErrorActionPreference = 'Stop'
+
+Import-Module -Name PSFramework
+Import-Module -Name dbatools
+
+$PSDefaultParameterValues['*-Dba*:EnableException'] = $true
+$PSDefaultParameterValues['*-Dba*:Confirm'] = $false
+
+try {
+
+Write-PSFMessage -Level Host -Message 'Configure SQL Server instance service to enable Always On'
+Enable-DbaAgHadr -SqlInstance $SqlInstances -Force | Format-Table
+
+$availabilityGroupParameters = @{
+    Primary            = $SqlInstances[0]
+    Secondary          = $SqlInstances[1]
+    Name               = $AvailabilityGroupName
+    IPAddress          = $AvailabilityGroupIP
+    Port               = 24330                 # This is only needed if you have also run the 02_setup_availability_group.ps1 - as the other availability group already uses port 1433
+    Database           = $DatabaseName
+    ClusterType        = 'Wsfc'
+    ConfigureXESession = $true
+}
+
+# Special configuration for one of our clients:
+$availabilityGroupParameters['DtcSupport'] = $true
+$availabilityGroupParameters['ConnectionModeInSecondaryRole'] = 'AllowAllConnections'
+if ($EndpointUrls.Count -gt 0) {
+    $availabilityGroupParameters['EndpointUrl'] = $EndpointUrls
+}
+
+Write-PSFMessage -Level Host -Message 'Create Always On Availability Group with automatic seeding'
+$availabilityGroupParameters['SeedingMode'] = 'Automatic'
+New-DbaAvailabilityGroup @availabilityGroupParameters | Format-Table
+Get-DbaAgReplica -SqlInstance $SqlInstances[0] -AvailabilityGroup $AvailabilityGroupName | Format-Table
+Get-DbaAgDatabase -SqlInstance $SqlInstances -AvailabilityGroup $AvailabilityGroupName -Database $DatabaseName | Format-Table
+
+
+# This is optional just to show some more features:
+
+Write-PSFMessage -Level Host -Message 'waiting...'
+Start-Sleep -Seconds 30
+
+Write-PSFMessage -Level Host -Message 'Drop Always On Availability Group'
+$null = Remove-DbaAvailabilityGroup -SqlInstance $SqlInstances[0] -AvailabilityGroup $AvailabilityGroupName
+$null = Remove-DbaDatabase -SqlInstance $SqlInstances[1] -Database $DatabaseName
+$null = Get-DbaEndpoint -SqlInstance $SqlInstances -Type DatabaseMirroring | Remove-DbaEndpoint -Confirm:$false
+
+Write-PSFMessage -Level Host -Message 'waiting...'
+Start-Sleep -Seconds 30
+
+Write-PSFMessage -Level Host -Message 'Create Always On Availability Group with manual seeding'
+$availabilityGroupParameters['SeedingMode'] = 'Manual'
+$availabilityGroupParameters['SharedPath'] = $BackupPath
+New-DbaAvailabilityGroup @availabilityGroupParameters | Format-Table
+Get-DbaAgReplica -SqlInstance $SqlInstances[0] -AvailabilityGroup $AvailabilityGroupName | Format-Table
+Get-DbaAgDatabase -SqlInstance $SqlInstances -AvailabilityGroup $AvailabilityGroupName -Database $DatabaseName | Format-Table
+
+
+Write-PSFMessage -Level Host -Message 'Add database to Always On Availability Group with backup-restore and automatic seeding'
+$null = New-DbaDatabase -SqlInstance $SqlInstances[0] -Name TestDB
+$null = Backup-DbaDatabase -SqlInstance $SqlInstances[0] -Database TestDB -Path $BackupPath -Type Full | Restore-DbaDatabase -SqlInstance $SqlInstances[1] -NoRecovery
+$null = Backup-DbaDatabase -SqlInstance $SqlInstances[0] -Database TestDB -Path $BackupPath -Type Log | Restore-DbaDatabase -SqlInstance $SqlInstances[1] -Continue -NoRecovery
+Add-DbaAgDatabase -SqlInstance $SqlInstances[0] -AvailabilityGroup $AvailabilityGroupName -Database TestDB -Secondary $SqlInstances[1] -SeedingMode Automatic | Format-Table
+Get-DbaAgDatabase -SqlInstance $SqlInstances -AvailabilityGroup $AvailabilityGroupName -Database TestDB | Format-Table
+
+
+Write-PSFMessage -Level Host -Message 'finished'
+
+} catch { Write-PSFMessage -Level Warning -Message 'failed' -ErrorRecord $_ }
